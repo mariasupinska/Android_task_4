fca56b1cbd63756c4e0b49d35ea75631b497dc91
mariasupinska <marysupinska@gmail.com>
2022-10-22 18:57:12 +0200

    Updated TaskListFragment class.

--- app/src/main/java/pl/edu/pb/todoapp/TaskListFragment.java
+++ app/src/main/java/pl/edu/pb/todoapp/TaskListFragment.java
@@ -1,8 +1,75 @@
 package pl.edu.pb.todoapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class TaskListFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private TaskAdapter adapter;
     public static final String KEY_EXTRA_TASK_ID = "KEY_EXTRA_TASK_ID";
 
+    private void updateView() {
+        TaskStorage taskStorage = TaskStorage.getInstance();
+        List<Task> tasks = taskStorage.getTasks();
+
+        if ( adapter == null ) {
+            adapter = new TaskAdapter(tasks);
+            recyclerView.setAdapter(adapter);
+        } else {
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.task_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateView();
+        return view;
+    }
+
+    private class TaskHolder extends RecyclerView.ViewHolder {
+        public TaskHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_task, parent, false));
+        }
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder> {
+        private List<Task> tasks;
+
+        public TaskAdapter(List<Task> tasks) {
+            this.tasks = tasks;
+        }
+
+        @NonNull
+        @Override
+        public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new TaskHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return tasks.size();
+        }
+    }
 }
