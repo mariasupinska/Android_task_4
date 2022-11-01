0c305ef20c6c9f059c80bc9e7095e0e4f960053e
mariasupinska <marysupinska@gmail.com>
2022-11-01 21:28:42 +0100

    New DatePicker for a Task.

--- app/src/main/java/pl/edu/pb/todoapp/TaskFragment.java
+++ app/src/main/java/pl/edu/pb/todoapp/TaskFragment.java
@@ -1,5 +1,6 @@
 package pl.edu.pb.todoapp;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,10 +15,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.UUID;
 
 public class TaskFragment extends Fragment {
     private Task task;
+    private final Calendar calendar = Calendar.getInstance();
+    private EditText dateField;
     private static final String ARG_TASK_ID = "ARG_TASK_ID";
 
     @Override
@@ -63,12 +70,6 @@ public class TaskFragment extends Fragment {
                 });
             }
 
-            Button dateButton = view.findViewById(R.id.task_date);
-            if(dateButton != null){
-                dateButton.setText(task.getDate().toString());
-                dateButton.setEnabled(false);
-            }
-
             CheckBox doneCheckBox = view.findViewById(R.id.task_done);
             if(doneCheckBox != null){
                 doneCheckBox.setChecked(task.isDone());
@@ -76,8 +77,30 @@ public class TaskFragment extends Fragment {
                     task.setDone(isChecked);
                 });
             }
+
+            dateField = view.findViewById(R.id.task_date);
+            DatePickerDialog.OnDateSetListener date = (view12, year, month, day) -> {
+              calendar.set(Calendar.YEAR, year);
+              calendar.set(Calendar.MONTH, month);
+              calendar.set(Calendar.DAY_OF_MONTH, day);
+              setupDateFieldValue(calendar.getTime());
+              task.setDate(calendar.getTime());
+            };
+
+            dateField.setOnClickListener(view1 -> {
+                new DatePickerDialog(getContext(), date, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            });
+            setupDateFieldValue(task.getDate());
         }
 
         return view;
+    }
+
+    private void setupDateFieldValue(Date date) {
+        Locale locale = new Locale("pl", "PL");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", locale);
+        dateField.setText(dateFormat.format(date));
     }
 }
