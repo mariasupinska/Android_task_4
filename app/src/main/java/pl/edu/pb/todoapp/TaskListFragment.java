f84ffccba13083e24f784cde6653f5d7bde5e216
mariasupinska <marysupinska@gmail.com>
2022-11-02 16:11:55 +0100

    New Menu element.

--- app/src/main/java/pl/edu/pb/todoapp/TaskListFragment.java
+++ app/src/main/java/pl/edu/pb/todoapp/TaskListFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -39,6 +41,18 @@ public class TaskListFragment extends Fragment {
     public void onResume() {
         super.onResume();
         updateView();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_task_menu, menu);
     }
 
     @Override
